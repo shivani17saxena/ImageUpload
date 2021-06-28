3ec0d0b8ce7efbd1fd4cd628710e9e9e6c391797
shivani saxena <shivani.saxena95@gmail.com>
2021-06-28 13:16:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, {Component} from'react';
+import './App.css'
+
+export class App extends Component{
+
+  state = {
+    profileImg : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+  }
+
+  imageHandler = (e) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if(reader.readyState === 2 ){
+        this.setState({ profileImg : reader.result })
+      }
+    }
+    reader.readAsDataURL(e.target.files[0])
+  }
+
+  render(){
+    const {profileImg} = this.state;
+  
+    return(
+      <div className="page">
+        <div className="container">
+          <h1 className="heading">Add your Image</h1>
+          <div className="img-holder">
+            <img src={profileImg} alt="" id="img" className="img" />
+          </div>
+          <input type="file" name="iage-upload" id="input" accept="image/*" onChange={this.imageHandler} />
+          <div className="label">
+            <label htmlFor="input" className="image-upload">
+              <i className="material-icons">add_photo_alternate</i>
+              Choose your photo
+            </label>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
